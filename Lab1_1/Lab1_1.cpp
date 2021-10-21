5610ab35c385ce45db294fea440afa3e219ab78e
Mateusz Chorzępa <w65461@student.wsiz.edu.pl>
2021-10-21 19:40:10 +0200

    Dodaj pliki projektów.

---
+++ Lab1_1/Lab1_1.cpp
@@ -0,0 +1,73 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	//program1
+	//string nralbumu = "w65461";
+
+	//cout << "nr albumu " << nralbumu << endl;
+
+	//program2
+	//int a;
+	//cout << "podaj liczbe: ", cin >> a;
+	//if (a % 2 == 0) {
+	//	cout << "liczba jest patrzysta." << endl;
+	//}
+	//else {
+	//	cout << "liczba nie jest parzysta." << endl;
+
+	//}
+	//program 3
+	//double a, b;
+	//cout << "podaj liczbe a: ", cin >> a;
+	//cout << "podaj liczbe b: ", cin >> b;
+	//if (a == b) {
+	//	cout << "liczby sa rowne" << endl;
+	//}
+	//else if (a > b) {
+	//	cout << "liczba a jest wieksza" << endl;
+	//}
+	//else {
+	//	cout << "liczba b jest wieksza" << endl;
+	//}
+	//program 4
+	//double a, b, c, d;
+	//cout << "podaj liczbe a: ", cin >> a;
+	//cout << "podaj liczbe b: ", cin >> b;
+	//cout << "podaj liczbe c: ", cin >> c;
+	//cout << "podaj liczbe d: ", cin >> d;
+	//cout << "srednia arytmetyczna wynosi: " << (a + b + c + d) / 4 << endl;
+	//program 5
+	/*float a, b;
+	int wybor;
+
+	cout << "podaj liczbe a: ", cin >> a;
+	cout << "podaj liczbe b: ", cin >> b;
+	cout << "\ndzialanie: \n1. suma\n2. roznica \n3.iloczyn \n4. iloraz \n";
+	cout << "wybierz opcje: ", cin >> wybor;
+	switch (wybor)
+	{
+	case 1:
+		cout << "suma: " << a << " + " << b << " = " << a + b << endl;
+		break;
+	case 2:
+		cout << "roznica: " << a << " - " << b << " = " << a - b << endl;
+		break;
+	case 3:
+		cout << "iloczyn: " << a << " * " << b << " = " << a * b << endl;
+		break;
+	case 4:
+		if (b != 0) {
+			cout << "iloraz: " << a << " / " << b << " = " << a / b << endl;
+			break;
+		}
+		else {
+			cout << "nie mozna dzielic przez 0";
+		}
+	default:
+		cout << "nie ma takiej opcji\n";
+		break;
+
+	}*/
+}
